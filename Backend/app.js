759ffb26d9d6e3d8d893b084399aac03cacaa675
Julien <gardier.julien@gmail.com>
2024-05-07 01:45:54 +0200

    cors in app.use

--- Backend/app.js
+++ Backend/app.js
@@ -20,6 +20,13 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTop
 .then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err));
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+})
+
 app.get('/', (req, res) => {
   res.send('Welcome to the API!');
 });
